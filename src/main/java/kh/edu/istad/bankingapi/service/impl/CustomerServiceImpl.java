438802ad1149a34bb42fa369a5c1b15629f5985d
Panharoth06 <panharothchheng0903@gmail.com>
2025-07-15 01:11:26 +0700

    feat: added kyc verifying, and set over limit base on customer segment

--- src/main/java/kh/edu/istad/bankingapi/service/impl/CustomerServiceImpl.java
+++ src/main/java/kh/edu/istad/bankingapi/service/impl/CustomerServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
