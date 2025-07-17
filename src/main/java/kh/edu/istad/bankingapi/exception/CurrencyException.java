03573f37d1167391357dc23094e8723a3d909ae3
Panharoth06 <panharothchheng0903@gmail.com>
2025-07-17 20:12:40 +0700

    add: implemented code to handle currency enum type

---
+++ src/main/java/kh/edu/istad/bankingapi/exception/CurrencyException.java
@@ -0,0 +1,30 @@
+package kh.edu.istad.bankingapi.exception;
+
+import org.springframework.beans.NotReadablePropertyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class CurrencyException {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleServiceException (HttpMessageNotReadableException e) {
+
+        ErrorResponse <?> errorResponse = ErrorResponse
+                .builder()
+                .message("Service Exception")
+                .code(HttpStatus.BAD_REQUEST.value())
+                .timeStamp(LocalDateTime.now())
+                .details(e.getMessage())
+                .build();
+
+        return ResponseEntity.badRequest().body(errorResponse);
+
+    }
+
+}
